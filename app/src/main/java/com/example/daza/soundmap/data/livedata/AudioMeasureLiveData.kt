820292300f9171addad15598009900f762393a2e
Dawid Zawrotny <zawrotny@protonmail.com>
2018-07-24 21:56:49 +0200

    Tmp change live data

--- app/src/main/java/com/example/daza/soundmap/data/livedata/AudioMeasureLiveData.kt
+++ app/src/main/java/com/example/daza/soundmap/data/livedata/AudioMeasureLiveData.kt
@@ -10,22 +10,23 @@ import kotlin.concurrent.scheduleAtFixedRate
  * Created by daza on 09.04.18.
  */
 class AudioMeasureLiveData(val context: Context) : MutableLiveData<Int>() {
-    val audioHelper = AudioHelper(context)
-    var isAudioRecording = false
+    //val audioHelper = AudioHelper(context)
+    //var isAudioRecording = false
     val timer = Timer("schedule", true)
 
 
     override fun onActive() {
         super.onActive()
-        audioHelper.recordAudio()
-        timer.scheduleAtFixedRate(1000, 1000) {
-            postValue(audioHelper.tmpEndedAt)
-        }
+        //audioHelper.recordAudio()
+//        timer.scheduleAtFixedRate(1000, 1000) {
+//            //postValue(audioHelper.tmpEndedAt)
+//            postValue(1)
+//        }
 
     }
 
     override fun onInactive() {
-        audioHelper.stopRecording()
+        //audioHelper.stopRecording()
     }
 
 }
