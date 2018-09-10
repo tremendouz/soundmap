6b57d5d0c4666868d2821a9d5b65c226fa5489d0
Dawid Zawrotny <zawrotny@protonmail.com>
2018-09-10 19:32:47 +0200

    Add FIFO to audio record

--- app/src/main/java/com/example/daza/soundmap/utils/AudioHelper.kt
+++ app/src/main/java/com/example/daza/soundmap/utils/AudioHelper.kt
@@ -6,8 +6,11 @@ import android.media.AudioRecord
 import android.media.AudioTrack
 import android.media.MediaRecorder
 import android.util.Log
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import java.nio.ByteBuffer
 import kotlin.concurrent.thread
+import kotlin.math.abs
 
 /**
  * Created by daza on 08.04.18.
@@ -17,6 +20,9 @@ class AudioHelper(val context: Context) {
 
     @Volatile
     var tmpEndedAt: Int = 0
+
+    @Volatile
+    lateinit var fifo: CustomFIFO.fifoAvarage
 
     val RECORD_TIME = 5000L
     val SAMPLE_RATE = 44100
@@ -169,4 +175,164 @@ class AudioHelper(val context: Context) {
         }
         return result
     }
+
+
+    /////////////////////////////////////////////////
+    // TMP MEASURE
+
+    fun testDb() {
+        fifo = CustomFIFO.fifoAvarage(10)
+
+        val SAMPLE_RATE = 44100
+        val CHANNEL = AudioFormat.CHANNEL_IN_MONO
+        val ENCODING = AudioFormat.ENCODING_PCM_16BIT
+        val SOURCE = MediaRecorder.AudioSource.DEFAULT
+
+        val dB: Double
+        val average = 0.0
+        val dbSumTotal = 0.0
+        val instant = 0.0
+        //val bufferSize = 8192
+        val bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT)
+
+        //val recorder = AudioRecord(SOURCE, SAMPLE_RATE, CHANNEL, ENCODING, bufferSize)
+        val recorder = AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLE_RATE,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, bufferSize)
+
+        val buffer = ShortArray(bufferSize)
+
+        isRecording = true
+        var current = 0
+        recorder.startRecording()
+//        launch {
+//            delay(1000L)
+//            isRecording = false
+//        }
+        thread {
+            Log.d(TAG, "Recording STARTED")
+            var counter = 0
+            while (isRecording) {
+                val xlen = recorder.read(buffer, 0, bufferSize)
+                // Log.d("Elo", "Max buffer ${buffer.max()}")
+                //val new_buffer = buffer
+//                for (i in 0 until xlen) {
+//                    if (buffer[i] < 0) {
+//                        //Log.d(TAG, "WTF probka mniejsza od 0")
+//                    } else {
+//                        //Log.d(TAG, "${buffer[i]}")
+//                        if (buffer[i] > current) {
+//                            current = buffer[i].toInt()
+//                            //Log.d(TAG, "MAX $current")
+//
+//                        }
+//                    }
+//                }
+                //Log.d(TAG, "${Arrays.toString(buffer)}")
+                val result = filterShortArray(buffer)
+                fifo.add(result)
+                //counter += 1
+                //Log.d(TAG, "Max RMS: $result")
+            }
+
+            recorder.stop()
+            recorder.release()
+//            val testar = shortArrayOf(1, 2, 5, 1107, 6526, 9759, 13711, 16295, 20985, 27061, 32767).toDouble()
+//            val testarfail = shortArrayOf(-1, -2, -5, -1107, -6526, -9759, -13711, -16295, -20985, -27061, -32767).toDouble()
+//            Log.d(TAG, "${Arrays.toString(testar)}")
+//            Log.d(TAG, "${Arrays.toString(testarfail)}")
+            Log.d(TAG, "Stopped recording")
+            //Log.d(TAG, " counter $counter")
+
+        }
+
+
+    }
+
+
+    fun bufferResults(){
+
+    }
+
+    fun DoubleArray.absmin(): Double{
+        var min :Double = abs(this[0])
+        for (i in 0 until this.size) {
+            if (min > abs(this[i]) && abs(this[i]) != 0.0) {
+                min = abs(this[i])
+            }
+        }
+        return min
+    }
+
+
+
+    fun filterShortArray(input: ShortArray): Double {
+//        val B_COEFFICIENT = doubleArrayOf(0.169994948147430,
+//                0.280415310498794,
+//                -1.120574766348363,
+//                0.131562559965936,
+//                0.974153561246036,
+//                -0.282740857326553,
+//                -0.152810756202003)
+        val B_COEFFICIENT = doubleArrayOf(0.25574113, -0.51148225, -0.25574113,  1.0229645 , -0.25574113,
+                -0.51148225,  0.25574113)
+        val A_COEFFICIENT = doubleArrayOf(1.00000000e+00, -4.01957618e+00,  6.18940644e+00, -4.45319890e+00,
+                1.42084295e+00, -1.41825474e-01,  4.35117723e-03)
+//        val A_COEFFICIENT = doubleArrayOf(1.00000000000000000,
+//                -2.12979364760736134,
+//                0.42996125885751674,
+//                1.62132698199721426,
+//                -0.96669962900852902,
+//                0.00121015844426781,
+//                0.04400300696788968)
+        //Log.d(TAG, "MIN ${input.toDouble().absmin()}")
+        //Log.d(TAG, "MAX ${input.toDouble().max()}")
+        val doubleArray = applyWeightingFilter(A_COEFFICIENT, B_COEFFICIENT, input.toDouble())
+
+        //Log.d(TAG, "ABSMIN ${doubleArray.absmin()}")
+        //Log.d(TAG, "MIN ${doubleArray.min()}")
+        //Log.d(TAG, "MAX ${doubleArray.max()}")
+        //Log.d(TAG, "test test ${Arrays.toString(doubleArray)}")
+        // Log.d(TAG, "MAX data ${input.max() }}")
+//        for(i in 0 until doubleArray.size){
+//            Log.d(TAG, "${doubleArray[i]}")
+//        }
+        var sum = 0.0
+        for (item in doubleArray) {
+            sum += item * item
+        }
+
+//
+//        --- TU JUZ WYNIK -----------
+        val rms = 20 * Math.log10(Math.sqrt(sum / doubleArray.size))
+        //Log.d(TAG, "RMS: $rms")
+        return rms
+    }
+
+    fun ShortArray.toDouble(): DoubleArray {
+        val doubleArray = DoubleArray(this.size)
+        for (i in 0 until this.size) {
+            //doubleArray[i] = this[i].toUInt().toDouble()
+            doubleArray[i] = this[i].toDouble()
+        }
+        return doubleArray
+    }
+
+    fun ShortArray.toUInt(): IntArray {
+        val uintArray = IntArray(this.size)
+        for (i in 0 until this.size) {
+            uintArray[i] = this[i].toUInt()
+        }
+        return uintArray
+    }
+
+    fun Short.toUInt() = toInt() and 0xffff
+
+
+//    fun Short.toUInt(): Int {
+//        return this.toInt() and 0xffff
+//    }
+
+    // END TMP
 }
--- app/src/main/java/com/example/daza/soundmap/utils/Utils.kt
+++ app/src/main/java/com/example/daza/soundmap/utils/Utils.kt
@@ -3,6 +3,7 @@ package com.example.daza.soundmap.utils
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
+import java.util.*
 
 /**
  * Created by daza on 18.05.18.
@@ -10,4 +11,130 @@ import android.arch.lifecycle.Observer
 fun <T> LiveData<T>.reObserve(owner: LifecycleOwner, observer: Observer<T>) {
     removeObserver(observer)
     observe(owner, observer)
+}
+
+
+open class CustomFIFO(size: Int): Queue<Double>{
+
+    val maxsize = size
+    val ring = DoubleArray(maxsize)
+    var index = 0
+
+    override fun contains(element: Double?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addAll(elements: Collection<Double>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun clear() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun element(): Double {
+        return ring[getCurrentIndex()]
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun remove(): Double {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override val size: Int
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    override fun containsAll(elements: Collection<Double>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun iterator(): MutableIterator<Double> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun remove(element: Double?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun removeAll(elements: Collection<Double>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun add(element: Double): Boolean {
+        return offer(element)
+    }
+
+    override fun offer(newElement: Double): Boolean {
+        ring[index]= newElement
+        incrIndex()
+        return true
+    }
+
+    override fun retainAll(elements: Collection<Double>): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun peek(): Double {
+        return ring[getCurrentIndex()]
+    }
+
+    override fun poll(): Double {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    protected fun incrIndex() {
+        index = nextIndex(index)
+    }
+
+    fun nextIndex(current: Int): Int {
+        return if (current + 1 >= ring.size) {
+            0
+        } else
+            current + 1
+    }
+
+    fun previousIndex(current: Int): Int {
+        return if (current - 1 < 0) {
+            ring.size - 1
+        } else
+            current - 1
+    }
+
+    fun getCurrentIndex(): Int{
+        if (index == 0) { return ring.size -1; }
+        else return index-1;
+    }
+
+    fun mAvarage(): Int {
+        var sum = 0.0;
+        for(i in 0 until maxsize){
+            sum += ring[i]
+        }
+        return (sum/maxsize).toInt()
+    }
+
+
+    class fifoAvarage(size: Int): CustomFIFO(size){
+        var numerator = 0.0
+        var value = 0.0
+
+        override fun add(element: Double): Boolean {
+            return offer(element)
+        }
+
+        override fun offer(newElement: Double): Boolean {
+            numerator -= ring[index]
+
+            val res = super.offer(newElement)
+
+            numerator += ring[getCurrentIndex()]
+            value = numerator / ring.size.toFloat()
+
+            return res
+        }
+    }
+
 }
